6111e337b16afb382e64fc9de55f36827cf02035
KevinYao <yaoqinwei@ones.ai>
2018-09-20 12:29:41 +0800

    code base.

---
+++ index.js
@@ -0,0 +1,3 @@
+import Validator from './src/lib/validator';
+
+export { Validator };
---
+++ src/lib/validator.js
@@ -0,0 +1,23 @@
+export default {
+  isEmpty(value) {
+    return !value;
+  },
+  isEmail(value) {
+    // test: https://regexr.com/
+    const reg = new RegExp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$");
+    return reg.test(value);
+  },
+  isPassword(value) {
+    return /^[\x21-\x7E]{8,32}$/.test(value);
+  },
+  isValidateCode(value) {
+    return /^\d{6}$/.test(value);
+  },
+  isPhone(value) {
+    const reg = new RegExp("^1[34578][0-9]{9}$"); // might changed in the future
+    return reg.test(value);
+  },
+  isNumber(value, isFloat) {
+    return isFloat ? /^[\d.]+$/.test(value) : /^\d+$/.test(value);
+  },
+};
